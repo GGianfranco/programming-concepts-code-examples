a9a5d18315eee3c82de6ae33207b56104034406a
GGianfranco <77826002+GGianfranco@users.noreply.github.com>
2022-02-09 18:03:50 +0800

    Addded comments for easier understanding of the algorithm.

--- javascript/sorting-algorithms/radix-sort.js
+++ javascript/sorting-algorithms/radix-sort.js
@@ -1,15 +1,16 @@
 const countingSort = require("./counting-sort");
 
 function radixSort(arr) {
-  const k = Math.max(...arr).toString().length;
-  let c = k - 1;
-  let digitKeys = new Map();
+  const k = Math.max(...arr).toString().length; // Get max number of digits of the elements in the array.
+  let c = k - 1; // Decrements the value because arrays are zero-based.
+  let digitKeys = new Map(); // Initializes a Map data structure to store our digit-by-digit reference. Digit of element will serve as key and the whole element itself will serve as the value.
 
+  // Counting sort digit-by-digit basis. Last digit counting sort would yield the sorted array.
   while (c >= 0) {
-    // Use the current digit as key for each element.
     arr.map((e) => {
-      let d = Number(e.toString().padStart(k, 0).charAt(c));
-      const v = digitKeys.get(d) || [];
+      // Iterate through each element in the unsorted array.
+      let d = Number(e.toString().padStart(k, 0).charAt(c)); // Pad each number with leading zeros. { "d": [100] }
+      const v = digitKeys.get(d) || []; // ❓ My question is why the value is an array instead od directly the element. 👈🏻 It is because for every values that have the same keys, we only need to save a signle key for them. Now next question is how do we visualize this algorithm. My answer to this is we can use CS Dojo's one whitebaord visualization provided.
       v.push(e);
       digitKeys.set(d, v);
     });
@@ -17,14 +18,14 @@ function radixSort(arr) {
     // Use counting sort to sort the keys.
     const sortedKeys = countingSort([...digitKeys.keys()]);
 
-    // Sort the elements using the sorted keys as reference.
+    // Let us sort the original array based on our sorted keys. Let us overwrite the original array to not utilize another extra space of O(n).
     arr = [];
     sortedKeys.map((key) => {
       const values = digitKeys.get(key);
       arr.push(...values);
     });
 
-    digitKeys.clear();
+    digitKeys.clear(); // Clear the keys from the previous digit to prepare for the next digit.
     --c;
   }
 
